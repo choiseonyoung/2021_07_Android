3e56052ef31890cb81a5c2fdd860565b740c423c
choiseonyoung <bviibb@naver.com>
2021-08-17 15:25:21 +0900

    네이버 영화 test2

---
+++ App_06_Movie/app/src/main/java/com/choiseonyoung/movie/config/NaverAPI_Sample.java
@@ -0,0 +1,13 @@
+package com.choiseonyoung.movie.config;
+
+public class NaverAPI_Sample {
+
+    /**
+     * Naver Open API 를 등록한 후
+     * ID, SECRET을 작성하여
+     * 클래스를 NaverAPI로 변경한 후 프로젝트를 build 하시오
+     */
+    public static final String NAVER_CLIENT_ID = "YOUR NAVER API ID";
+    public static final String NAVER_CLIENT_SECRET = "YOUR NAVER API SECRET";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+}
